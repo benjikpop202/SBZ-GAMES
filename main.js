f3b3bf361686ebac6013d6ea919aceff01d6c608
benjikpop202 <benjaminsobarzo2018@gmail.com>
2023-11-30 17:13:36 -0300

    nuevo diseño del menu

--- main.js
+++ main.js
@@ -6,16 +6,27 @@ principal.addEventListener("click", ()=>{
     document.getElementById("seccion-principal").style.display = "flex"
     document.getElementById("seccion-noticias").style.display = "none"
     document.getElementById("seccion-nosotros").style.display = "none"
+    principal.style.background = "blueviolet"
+    noticias.style.background = "transparent" 
+    nosotros.style.background = "transparent"
+
 })
 
 noticias.addEventListener("click", ()=>{
     document.getElementById("seccion-noticias").style.display = "flex"
     document.getElementById("seccion-principal").style.display = "none"
     document.getElementById("seccion-nosotros").style.display = "none"
+    noticias.style.background = "blueviolet"
+    principal.style.background = "transparent" 
+    nosotros.style.background = "transparent"
+    
 })
 
 nosotros.addEventListener("click", ()=>{
     document.getElementById("seccion-nosotros").style.display = "flex"
     document.getElementById("seccion-principal").style.display = "none"
     document.getElementById("seccion-noticias").style.display = "none"
+    nosotros.style.background = "blueviolet"
+    noticias.style.background = "transparent" 
+    principal.style.background = "transparent"
 })
